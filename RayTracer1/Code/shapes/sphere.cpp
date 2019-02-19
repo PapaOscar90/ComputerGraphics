d5f2126142dc5c13c6dca9af70c465c946969016
Philip Oetinger <p.oetinger@student.rug.nl>
2019-02-19 16:10:06 +0100

    add the mathmatical description for the code to be added

--- RayTracer1/Code/shapes/sphere.cpp
+++ RayTracer1/Code/shapes/sphere.cpp
@@ -6,46 +6,61 @@ using namespace std;
 
 Hit Sphere::intersect(Ray const &ray)
 {
-    /****************************************************
-    * RT1.1: INTERSECTION CALCULATION
-    *
-    * Given: ray, position, r
-    * Sought: intersects? if true: *t
-    *
-    * Insert calculation of ray/sphere intersection here.
-    *
-    * You have the sphere's center (C) and radius (r) as well as
-    * the ray's origin (ray.O) and direction (ray.D).
-    *
-    * If the ray does not intersect the sphere, return false.
-    * Otherwise, return true and place the distance of the
-    * intersection point from the ray origin in *t (see example).
-    ****************************************************/
-
-    // place holder for actual intersection calculation
+    /* *********************************************************************
+     * RT1.1: INTERSECTION CALCULATION
+     *
+     * Given: ray, position, r
+     * Sought: intersects? if true: *t
+     *
+     * Insert calculation of ray/sphere intersection here.
+     *
+     * You have the sphere's center (C) and radius (r) as well as
+     * the ray's origin (ray.O) and direction (ray.D).
+     *
+     * If the ray does not intersect the sphere, return false.
+     * Otherwise, return true and place the distance of the
+     * intersection point from the ray origin in *t (see example).
+     *  (e+td-c)*(e+td-c) - R^2 = 0
+     * Where e is the origin, t the distance, d the vector of the ray
+     * and c the center of the sphere
+     * 
+     * This expands into the quadratic solution form, which looks at the
+     * discriminant. Specifically if:
+     * 
+     * D = (d*(e-c))^2 - (d*d)((e-c)*(e-c)- R^2) = 0 -> single solution, ray grazes
+     *                                           > 0 -> Two strikes, get closest
+     *                                           < 0 -> The ray misses the sphere
+     * 
+     * The solution for t becomes:
+     * 
+     *  t = (-d*(e-c) +/- sqrt(D)) / (d*d)
+     ***************************************************************************/
 
     Vector OC = (position - ray.O).normalized();
-    if (OC.dot(ray.D) < 0.999) {
+    if (OC.dot(ray.D) < 0.999)
+    {
         return Hit::NO_HIT();
     }
     double t = 1000;
 
-    /****************************************************
-    * RT1.2: NORMAL CALCULATION
-    *
-    * Given: t, C, r
-    * Sought: N
-    *
-    * Insert calculation of the sphere's normal at the intersection point.
-    ****************************************************/
-
+    /**************************************************************************
+     * RT1.2: NORMAL CALCULATION
+     *
+     * Given: t, C, r
+     * Sought: N
+     *
+     * The normal at point p ()= e+td) on the surface, is given by:
+     * 
+     * N = 2(p-c) 
+     * 
+     **************************************************************************/
     Vector N /* = ... */;
 
-    return Hit(t,N);
+    return Hit(t, N);
 }
 
 Sphere::Sphere(Point const &pos, double radius)
-:
-    position(pos),
-    r(radius)
-{}
+    : position(pos),
+      r(radius)
+{
+}

44d35132f1fd816a2264e6d397c895e6885e75c2
Troy Karan Harrison <NoxPardalis@protonmail.com>
2019-02-23 13:09:57 +0100

    Fix index access bug

--- RayTracer1/Code/shapes/mesh.cpp
+++ RayTracer1/Code/shapes/mesh.cpp
@@ -15,12 +15,13 @@ Hit Mesh::intersect(Ray const &ray) {
   // Looking for the closest hit
   for (size_t i = 0; i < model.numTriangles(); ++i) {
     Point vertex1 =
-        Point(vertices[i].x, vertices[i].y, vertices[i].z) + translation;
-    Point vertex2 =
-        Point(vertices[i + 1].x, vertices[i + 1].y, vertices[i + 1].z) +
+        Point(vertices[3 * i].x, vertices[3 * i].y, vertices[3 * i].z) +
         translation;
-    Point vertex3 = Point(vertices[i + 2].x, vertices[i + 2].y,
-                          vertices[i + 2].z) = translation;
+    Point vertex2 = Point(vertices[3 * i + 1].x, vertices[3 * i + 1].y,
+                          vertices[3 * i + 1].z) +
+                    translation;
+    Point vertex3 = Point(vertices[3 * i + 2].x, vertices[3 * i + 2].y,
+                          vertices[3 * i + 2].z) = translation;
     Triangle triangle(vertex1, vertex2, vertex3);
 
     Hit intersection(triangle.intersect(ray));

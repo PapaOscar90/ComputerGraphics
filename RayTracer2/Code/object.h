cfa966bad6226687cc6eaf93c3322c32e365ffe9
Philip Oetinger <p.oetinger@student.rug.nl>
2019-03-11 12:21:31 +0100

    Add TextureCoordinates struct with virtual compute func

--- RayTracer2/Code/object.h
+++ RayTracer2/Code/object.h
@@ -12,14 +12,22 @@
 class Object;
 typedef std::shared_ptr<Object> ObjectPtr;
 
+typedef struct TextureCoordinates{
+  float x;
+  float y;
+} TextureCoordiantes;
+
 class Object {
 public:
   Material material;
+  
 
   virtual ~Object() = default;
 
   virtual Hit intersect(Ray const &ray) = 0; // must be implemented
                                              // in derived class
+
+  virtual TextureCoordiantes findTextureCoords(Point hit, Object &self);
 };
 
 #endif
